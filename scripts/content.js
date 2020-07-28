54b7b3039a8c8bc898e62313e53404b467497b80
Haikel Fazzani <haikel.fazzani@hotmail.com>
2020-07-28 16:10:10 +0100

    update app

--- scripts/content.js
+++ scripts/content.js
@@ -25,25 +25,32 @@ function showDialog (event) {
     let bgColor = window.getComputedStyle(elementMouseIsOver, null).getPropertyValue("background-color");
     let fontFamily = window.getComputedStyle(elementMouseIsOver, null).getPropertyValue("font-family");
 
+    let fontSize = window.getComputedStyle(elementMouseIsOver, null).getPropertyValue("font-size");
+    let fontWeight = window.getComputedStyle(elementMouseIsOver, null).getPropertyValue("font-weight");
+    let fontStyle = window.getComputedStyle(elementMouseIsOver, null).getPropertyValue("font-style");
+
     if (mouseX + 250 > docWidth) {
-      mouseX = mouseX - 250;
+      mouseX = docWidth - 250;
     }
 
-    if (mouseY + 250 > docHeight) {
-      mouseY = mouseY - 50;
+    if (mouseY + 150 > docHeight) {
+      mouseY = docHeight - 150;
     }
 
     createDialog(
       fontFamily,
+      fontSize,
+      fontWeight,
+      fontStyle,
       Utils.rgbToHex(color.trim()),
       Utils.rgbToHex(bgColor.trim()),
-      mouseX,
-      mouseY
+      mouseX - 13,
+      mouseY + 13
     );
   }
 }
 
-function createDialog (fontFamily, color, bgColor, posX, posY) {
+function createDialog (fontFamily, fontSize, fontWeight, fontStyle, color, bgColor, posX, posY) {
   Utils.removeDomElement(dialogElementId);
   const div = document.createElement('div');
 
@@ -51,12 +58,29 @@ function createDialog (fontFamily, color, bgColor, posX, posY) {
   div.style.left = posX + 'px';
   div.style.top = posY + 'px';
 
-  div.innerHTML = `<span class="wi-100 he-50 di-flex p-15" style="font-family:${fontFamily}">${fontFamily}</span>
-  <div class="wi-100 he-50 di-flex bd-top" id="show-color-item">
-    <span class="wi-50 di-flex p-15" 
-    style="background:${color}; color:${Utils.isColorLight(color) ? '#000' : color}">C: ${color}</span>
-    <span class="wi-50 di-flex p-15" 
+  div.innerHTML = `<pre class="wi-100 he-75 di-flex m-0 pa-15" style="font-family:${fontFamily}">
+    ${fontFamily}
+  </pre>
+  
+  <div class="wi-100 he-25 di-flex bd-top" id="show-color-item">
+    <span class="wi-50 di-flex pa-5 bd-right" 
+    style="background:${color}; color:${Utils.isColorLight(color) ? '#000' : '#fff'}">C: ${color}</span>
+
+
+    <span class="wi-50 di-flex pa-5 bd-left" 
     style="background:${bgColor}; color: ${Utils.isColorLight(bgColor) ? '#000' : '#fff'}">B: ${bgColor}</span>
+  </div>
+  
+  
+  <div class="wi-100 he-25 di-flex bd-top" id="show-color-item">
+    <span class="di-flex pa-5 bd-right">F: ${fontSize}</span>
+    <span class="di-flex pa-5 bd-right">W: ${fontWeight}</span>
+    <span class="di-flex pa-5 bd-left">W: ${fontStyle}</span>
+  </div>
+  
+  <div class="wi-100 he-25 di-flex bd-top" id="show-color-item">
+  <a class="wi-50 di-flex pa-5 txt-muted fs-10" target="_blank" href="https://github.com/Chromo-lib/detector">Via Detector v1</span>
+    <a class="wi-50 di-flex pa-5 txt-muted fs-10" target="_blank" href="https://github.com/Chromo-lib/detector/issues">Issues</span>
   </div>`;
 
   document.body.appendChild(div);
